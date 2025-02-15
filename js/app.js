496f81745a875b25a27cdd3e52098c9cd4836250
IreneC16 <you@example.com>
2025-02-14 23:49:49 -0300

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,62 @@
+function comprar() {
+    let tipoIngresso = document.getElementById('tipo-ingresso').value; 
+    let quantidade = parseInt(document.getElementById('qtd').value);
+   
+    if(tipoIngresso == 'pista') {
+        comprarPista(quantidade);
+    } else if(tipoIngresso == 'superior') {
+        comprarCSuperior(quantidade);
+    } else {
+        comprarCInferior(quantidade);
+    }
+
+}
+
+function comprarPista(quantidade) {
+    let quantidadePista = parseInt(document.getElementById('qtd-pista').textContent);
+    
+    if(quantidade > quantidadePista) {
+        alert('Quantidade de ingressos indisponível para tipo Pista');
+        limpar();
+    } else {
+        quantidadePista = quantidadePista - quantidade;
+        document.getElementById('qtd-pista').textContent = quantidadePista;
+        alert('Compra realizada com sucesso!');
+        limpar();
+    }
+
+}
+
+function comprarCSuperior(quantidade) {
+    let quantidadeCSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+
+    if(quantidade > quantidadeCSuperior) {
+        alert('Quantidade de ingressos indisponível para tipo Cadeira superior');
+        limpar();
+    } else{
+        quantidadeCSuperior = quantidadeCSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = quantidadeCSuperior;
+        alert('Compra realizada com sucesso!');
+        limpar();
+    }
+    
+}
+
+function comprarCInferior(quantidade) {
+    let quantidadeCInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    if (quantidade > quantidadeCInferior) {
+        alert('Quantidade de ingressos indisponível para tipo Cadeira inferior');
+        limpar();
+    } else {
+        quantidadeCInferior = quantidadeCInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = quantidadeCInferior;
+        alert('Compra realizada com sucesso!');
+        limpar();
+    }
+
+}
+
+function limpar() {
+    document.getElementById('qtd').value = '';
+}
